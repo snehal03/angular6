056ee502ca258a085e07b2fbdd6cfe9e18cd63d1
snehal <snehal>
2018-09-28 11:54:05 +0530

    changed path

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -7,8 +7,8 @@ import { Routes } from '@angular/router';
 export const Full_ROUTES: Routes = [
   {
     path: 'form',
-    // loadChildren: 'src/app/modules/file-upload/file-upload.module#FileUploadModule'
-    loadChildren: '../../modules/forms/form-page.module#FormPageModule'
+    loadChildren: 'src/app/modules/forms/form-page.module#FormPageModule'
+    // loadChildren: '../../modules/forms/form-page.module#FormPageModule'
   },
   {
     path: 'fileUpload',
